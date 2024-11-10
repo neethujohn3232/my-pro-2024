d272199efaa0c3ba4c845a0fecc46e2539eb3e89
neethujohn3232 <neethujohnck32@gmail.com>
2024-11-10 11:10:34 +0530

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,51 +1,32 @@
-{{-- @extends('layouts.app') --}}
-
-
 @extends('layout.mainlayout')
-@section('content')	
-<div class="page-wrapper">
-			<div class="content container-fluid">
-			
-				<!-- Page Header -->
-				<div class="page-header">
-					<div class="row">
-						<div class="col-12">
-                            
-							<h3 class="page-title">Welcome Admin!</h3>
-                            @if (session('status'))
+@section('content')
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+
+            <!-- Page Header -->
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-12">
+
+                        <h3 class="page-title">Welcome Admin!</h3>
+                        @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
-    
+
                         {{ __('You are logged in!') }}
-						</div>
+                    </div>
 
-					</div>
-				</div>
-				<!-- /Page Header -->
-				
-			
-			</div>
-		</div> 
-	</div>	
-    <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
+            <!-- /Page Header -->
+
+
         </div>
     </div>
-</div>
-@endsection 
+    </div>
+    <div class="container">
+       
+    </div>
+@endsection
